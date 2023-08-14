dd45626306ee076c5c97651e58e644adb1873d75
Braulio Viveros <bviveros@csuchico.edu>
2023-08-14 11:16:45 -0600

    idk

---
+++ main.js
@@ -0,0 +1,11 @@
+// 1. Deposit some money
+// 2. determine number of lines to bet on 
+// 3. collect a bet amount
+// 4. spin the reels 
+// 5. determine if there is a win
+// 6. if there is a win, add the win amount to the balance
+// 7. play again or cash out
+
+function deposit_money(){
+    
+}
